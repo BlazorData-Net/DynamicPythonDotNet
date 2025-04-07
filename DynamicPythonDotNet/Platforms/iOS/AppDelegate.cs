b22a97ee50a3a7716a241de630ffde7d05aeb20e
AlanKeeper <48694542+AlanKeeper@users.noreply.github.com>
2025-04-06 17:32:20 -0700

    Add project files.

---
+++ DynamicPythonDotNet/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DynamicPythonDotNet
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
